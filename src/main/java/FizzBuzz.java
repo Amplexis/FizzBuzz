70d1e8a33b2ba0f037dbe4074470e5d53f2805bf
Amplexis <kevin.hamlin@gmail.com>
2019-06-20 13:10:47 -0400

    Passed first FizzBuzz test

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,5 @@
 public class FizzBuzz {
+    public String fizz(int inputNumber) {
+      return "1";
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,8 +1,15 @@
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
+    @Test
+    public void whenPassedANumberFizzBuzzWillReturnThatNumber(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.fizz(1));
+
+    }
 
 
 
